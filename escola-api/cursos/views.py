1504396ed24f9c44ea719ba69989858dea94372b
andre-fandrade <kodec_andrade@hotmail.com>
2020-11-19 12:59:09 -0300

    Criando APIViews, GET / POST - Termino DRF Básico.

--- escola-api/cursos/views.py
+++ escola-api/cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -14,12 +15,29 @@ class CursosAPIView(APIView):
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        print(type(serializer.data))
+        return Response({"Mensagem":"Criado com sucesso!",
+                         "Titulo": serializer.data['titulo'],
+                         "Item Criado": serializer.data},
+                         status.HTTP_201_CREATED)
+
 
 class AvaliacaoAPIView(APIView):
     """
     API DE Avaliações André
     """
     def get(self, request):
+        print(dir(request))
         avaliacoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
