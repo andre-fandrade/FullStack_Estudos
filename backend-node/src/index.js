d5d44ce35fe5866a7c316f07a8bd4e35be506ea2
andre-fandrade <kodec_andrade@hotmail.com>
2020-11-22 14:31:28 -0300

    Config. Controller e Router, cadastrando uma nova tarefa.

--- backend-node/src/index.js
+++ backend-node/src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
-server.get('/teste', (req, res) => {
-    res.send('Tudo cers com nossa API.');
-});
+const TaskRoutes = require('./routes/TaskRoutes')
+server.use('/task', TaskRoutes);
 
 server.listen(3000, () => {
     console.log('API ONLINE');
