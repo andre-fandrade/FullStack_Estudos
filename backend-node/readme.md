4ea7c9782b8194ce0d0237a71af156caed3d5ad2
andre-fandrade <kodec_andrade@hotmail.com>
2020-11-23 10:03:27 -0300

    Configuração de Middleware de Validação.

--- backend-node/readme.md
+++ backend-node/readme.md
@@ -1,12 +1,24 @@
 **_npm init -y_** (Cria um projeto Node no diretório.)
 
+---
+
 **_npm install express --save_** (O Express é um framework para aplicativo da web do Node.js mínimo e flexível que fornece um conjunto robusto de recursos para aplicativos web e móvel.)
 
+---
+
 **_npm install -g nodemon_ [Versão Global]**
- 
+
 **_npm install nodemon -D_ [Dependência de Desenvolvimento]** (Nodemon é um utilitário que monitorará quaisquer alterações na sua fonte e reiniciará automaticamente o servidor.)
 
+---
+
 **_npm install mongoose --save_** (Conexão com banco de dados MongoDB.)
+
+---
+
+**_npm install date-fns --save_** (Módulo de tratamento e verificação de Data e Hora.)
+
+---
 
 _npm install express **--save**_ `"dependencies": {
     "express": "^4.17.1"
---
+++ backend-node/src/middleware/TaskValidation.js
@@ -0,0 +1,39 @@
+const TaskModel = require('../model/TaskModel');
+const { isPast } = require('date-fns');
+const { exists } = require('../model/TaskModel');
+
+const TaskValidation = async (req, res, next) => {
+
+     const { macaddress, type, title, description, when } = req.body;
+
+    if (!macaddress)
+        return res.status(400).json({ error: 'Macaddress é obrigatório.' });
+    else if (!type)
+        return res.status(400).json({ error: 'Tipo é obrigatório.' });
+    else if (!title)
+        return res.status(400).json({ error: 'Título é obrigatório.' });
+    else if (!description)
+        return res.status(400).json({ error: 'Descição é obrigatório.' });
+    else if (!when)
+        return res.status(400).json({ error: 'Data e Hora são obrigatórios.' });
+    else if (isPast(new Date(when)))
+        return res.status(400).json({ error: 'Data e Hora não podem estar no passado.' });
+
+    else {
+        let exists;
+
+        exists = await TaskModel.findOne({
+            'when': {'$eq': new Date(when)},
+            'macaddress': {'$in': macaddress}
+        })
+
+        if(exists) {
+            return res.status(400).json({ error: 'Já existe uma taréfa cadastrada nesse dia e horário.' });
+        }
+
+        next();
+    }
+
+}
+
+module.exports = TaskValidation;
