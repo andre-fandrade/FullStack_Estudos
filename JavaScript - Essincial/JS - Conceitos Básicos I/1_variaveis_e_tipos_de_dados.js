a653bf92420d49e63a9b6158656c1e72a6abdda4
andre-fandrade <kodec_andrade@hotmail.com>
2020-11-25 18:17:36 -0300

    JS - Tipos de dados.

---
+++ JavaScript - Essincial/JS - Conceitos Básicos I/1_variaveis_e_tipos_de_dados.js
@@ -0,0 +1,22 @@
+var nome1 = "André Felipe I";
+var nome2 = 'André Felipe II';
+
+nome1 = 42
+
+console.log(nome1);
+
+// Principais Tipos de Dados
+var nome = 'Andre'; // String
+var idade = 26; // Inteiro
+var altura = 1.80; // Float (Ponto Flutuante)
+var ativo = true; // Booleano
+var alunos = ["João", "Maria", "Gustavo"]; //Vetor (Array)
+var aluno = {
+    nome: 'Andre',
+    idade: 26,
+    ativo: true,
+    peso: 94.8
+}; // Objeto
+
+console.log('')
+
--- JavaScript - Essincial/minimo-js/script.js
+++ JavaScript - Essincial/minimo-js/script.js
@@ -3,23 +3,27 @@ function gritar() {
 }
 
 function perguntar(){
-	var nome;
+	let nome;
 	nome = prompt("Qual é o seu nome? ");
 	alert("Olá " + nome);
 }
 
 function mudar_texto(){
-	var h1 = document.getElementsByTagName("h1");
+	let h1 = document.getElementsByTagName("h1");
 
-	if(h1[0].innerText == "Geek University"){
-		h1[0].innerText = "Evolua seu lado geek!";	
+	if(h1[0].innerText === "Geek University"){
+		h1[0].innerText = "Evolua seu lado geek!";
+
 	}else{
 		h1[0].innerText = "Geek University";
+
 	}
 }
 
 function incrementar(){
-	var p1 = document.getElementById("p1");
+	const p1 = document.getElementById("p1");
 
 	p1.innerText = parseInt(p1.innerText) + 1;
 }
+
+console.log(document.getElementsByTagName('h1')[1])
